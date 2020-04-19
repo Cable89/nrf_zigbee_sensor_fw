21a5edf100461d19f4311a926b740d13355b58bf
Smith <Oystein.Smith@nordicsemi.no>
2020-04-19 03:06:23 +0200

    Added gitignore

--- pca10100/blank/ses/Output/zigbee_multi_sensor_pca10100 Release/Obj/sensorsim.d
+++
@@ -1,5 +0,0 @@
-Output/zigbee_multi_sensor_pca10100\ Release/Obj/sensorsim.o: \
- C:\nRF5_SDK_for_Thread_and_Zigbee_v4.0.0_dc7186b\components\libraries\sensorsim\sensorsim.c \
- C:\nRF5_SDK_for_Thread_and_Zigbee_v4.0.0_dc7186b\components\libraries\sensorsim\sensorsim.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30b/include/stdint.h \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 4.30b/include/stdbool.h
--- pca10100/blank/ses/Output/zigbee_multi_sensor_pca10100 Release/Obj/ses_startup_nrf52833.d
+++
@@ -1,2 +0,0 @@
-Output/zigbee_multi_sensor_pca10100\ Release/Obj/ses_startup_nrf52833.o: \
- C:\nRF5_SDK_for_Thread_and_Zigbee_v4.0.0_dc7186b\modules\nrfx\mdk\ses_startup_nrf52833.s
