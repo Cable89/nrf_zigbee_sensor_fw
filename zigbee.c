55feb2a3ad43cacf7d23634ff8dbceede9d817be
Smith <Oystein.Smith@nordicsemi.no>
2020-04-19 03:07:11 +0200

    Moved first zigbee function to separate file

---
+++ zigbee.c
@@ -0,0 +1,64 @@
+//#include "zboss_api.h"
+//#include "zboss_api_core.h"
+//#include "zb_mem_config_med.h"
+//#include "zb_zcl_reporting.h"
+//#include "zb_error_handler.h"
+//#include "zigbee_helpers.h"
+
+#include "zb_multi_sensor.h"
+#include "zigbee.h"
+
+sensor_device_ctx_t m_dev_ctx;
+
+/**@brief Function for initializing all clusters attributes.
+ */
+void multi_sensor_clusters_attr_init(void)
+{
+    /* Basic cluster attributes data */
+    m_dev_ctx.basic_attr.zcl_version   = ZB_ZCL_VERSION;
+    m_dev_ctx.basic_attr.app_version   = SENSOR_INIT_BASIC_APP_VERSION;
+    m_dev_ctx.basic_attr.stack_version = SENSOR_INIT_BASIC_STACK_VERSION;
+    m_dev_ctx.basic_attr.hw_version    = SENSOR_INIT_BASIC_HW_VERSION;
+
+    /* Use ZB_ZCL_SET_STRING_VAL to set strings, because the first byte should
+     * contain string length without trailing zero.
+     *
+     * For example "test" string wil be encoded as:
+     *   [(0x4), 't', 'e', 's', 't']
+     */
+    ZB_ZCL_SET_STRING_VAL(m_dev_ctx.basic_attr.mf_name,
+                          SENSOR_INIT_BASIC_MANUF_NAME,
+                          ZB_ZCL_STRING_CONST_SIZE(SENSOR_INIT_BASIC_MANUF_NAME));
+
+    ZB_ZCL_SET_STRING_VAL(m_dev_ctx.basic_attr.model_id,
+                          SENSOR_INIT_BASIC_MODEL_ID,
+                          ZB_ZCL_STRING_CONST_SIZE(SENSOR_INIT_BASIC_MODEL_ID));
+
+    ZB_ZCL_SET_STRING_VAL(m_dev_ctx.basic_attr.date_code,
+                          SENSOR_INIT_BASIC_DATE_CODE,
+                          ZB_ZCL_STRING_CONST_SIZE(SENSOR_INIT_BASIC_DATE_CODE));
+
+    m_dev_ctx.basic_attr.power_source = SENSOR_INIT_BASIC_POWER_SOURCE;
+
+    ZB_ZCL_SET_STRING_VAL(m_dev_ctx.basic_attr.location_id,
+                          SENSOR_INIT_BASIC_LOCATION_DESC,
+                          ZB_ZCL_STRING_CONST_SIZE(SENSOR_INIT_BASIC_LOCATION_DESC));
+
+
+    m_dev_ctx.basic_attr.ph_env = SENSOR_INIT_BASIC_PH_ENV;
+
+    /* Identify cluster attributes data */
+    m_dev_ctx.identify_attr.identify_time        = ZB_ZCL_IDENTIFY_IDENTIFY_TIME_DEFAULT_VALUE;
+
+    /* Temperature measurement cluster attributes data */
+    m_dev_ctx.temp_attr.measure_value            = ZB_ZCL_ATTR_TEMP_MEASUREMENT_VALUE_UNKNOWN;
+    m_dev_ctx.temp_attr.min_measure_value        = ZB_ZCL_ATTR_TEMP_MEASUREMENT_MIN_VALUE_MIN_VALUE;
+    m_dev_ctx.temp_attr.max_measure_value        = ZB_ZCL_ATTR_TEMP_MEASUREMENT_MAX_VALUE_MAX_VALUE;
+    m_dev_ctx.temp_attr.tolerance                = ZB_ZCL_ATTR_TEMP_MEASUREMENT_TOLERANCE_MAX_VALUE;
+
+    /* Pressure measurement cluster attributes data */
+    m_dev_ctx.pres_attr.measure_value            = ZB_ZCL_ATTR_PRES_MEASUREMENT_VALUE_UNKNOWN;
+    m_dev_ctx.pres_attr.min_measure_value        = ZB_ZCL_ATTR_PRES_MEASUREMENT_MIN_VALUE_MIN_VALUE;
+    m_dev_ctx.pres_attr.max_measure_value        = ZB_ZCL_ATTR_PRES_MEASUREMENT_MAX_VALUE_MAX_VALUE;
+    m_dev_ctx.pres_attr.tolerance                = ZB_ZCL_ATTR_PRES_MEASUREMENT_TOLERANCE_MAX_VALUE;
+}
--- zigbee.h
+++ zigbee.h
@@ -1,7 +1,18 @@
 #ifndef ZIGBEE_H
 #define ZIGBEE_H
 
+//#include "zboss_api.h"
+//#include "zboss_api_core.h"
+//#include "zb_mem_config_med.h"
+//#include "zb_zcl_reporting.h"
+//#include "zb_error_handler.h"
+//#include "zigbee_helpers.h"
+
+#include "zb_multi_sensor.h"
+
+extern sensor_device_ctx_t m_dev_ctx;
+
+void multi_sensor_clusters_attr_init(void);
 
 
-
-#endif ZIGBEE_H
+#endif // ZIGBEE_H
